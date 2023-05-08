b899ea5765e791a1b0dfdf2690233647985ecba6
JuanB12 <jcamilobb03@gmail.com>
2023-05-08 09:55:33 -0500

    prueba lerma

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, session, json, jsonify, render_template, redirect
+
+from config.bd import bd, ma, app
+
+from model.Pacientes import Pacientes, Pacientes_Schema
+from model.Dentista import Dentista, Dentista_Schema
+from model.HistoriaClinica import Historia_Clinica, HistoriaC_Schema
+from model.Facturacion import Facturacion, Facturacion_Schema
+from model.Dientes import Dientes, Dientes_Schema
+from model.Agenda import Agenda, Agenda_Schema
+from model.Consultas import Consultas, Consultas_Schema
+
+
+agenda_schema = Agenda_Schema()
+agendas_schema = Agenda_Schema(many=True)
+consulta_schema = Consultas_Schema()
+consultas_schema = Consultas_Schema(many=True)
+dentista_schema = Dentista_Schema()
+dentistas_schema = Dentista_Schema(many=True)
+diente_schema = Dientes_Schema()
+dientes_Schema = Dientes_Schema(many=True)
+facturacion_schema = Facturacion_Schema()
+facturaciones_schema = Facturacion_Schema(many=True)
+hc_schema = HistoriaC_Schema()
+hcs_schema = HistoriaC_Schema(many=True)
+paciente_schema = Pacientes_Schema()
+pacientes_schema = Pacientes_Schema(many=True)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Hola Mundo!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
---
+++ config/bd.py
@@ -0,0 +1,14 @@
+# Importación de librerias a trabajar
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# odontograma es el nombre de mi BDD
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@localhost/odontograma"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.secret_key = "JuanBarrios12"
+
+bd = SQLAlchemy(app)
+ma = Marshmallow(app)
